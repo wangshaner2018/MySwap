33daf4b2d72b24a6ea7ef1e96d964639b12c889c
wang <wangll_job@hotmail.com>
2022-12-14 16:28:23 +0800

    1

---
+++ src/constants/lists.ts
@@ -0,0 +1,8 @@
+// the Uniswap Default token list lives here
+export const DEFAULT_TOKEN_LIST_URL = 'https://raw.githubusercontent.com/wangshaner2018/MySwap/master/tokenlist.json'
+
+export const DEFAULT_LIST_OF_LISTS: string[] = [
+  DEFAULT_TOKEN_LIST_URL
+  // 'http://tokenlist.vvpay.me/tokenlist.json', // kleros
+  // 'https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json'
+]
